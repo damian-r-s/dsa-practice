e685e94f4b24481efc79515b44ba88e531101da4
Damian Sobanski <damian.flexsoft@gmail.com>
2022-12-19 00:01:24 +0100

    Most profitable paths solution and unit tests

--- Python/DynamicProgramming/Matrix/MatrixMostProfitablePath.py
+++ Python/DynamicProgramming/Matrix/MatrixMostProfitablePath.py
@@ -1,34 +1,61 @@
-def MatrixNumberOfPaths(m, n, prifits):
+import unittest
+
+def MatrixPathMaximumProfit(m, n, profits):
     if m == 1 and n == 1:
-        return 1
+        return profits[0][0]
     
-    paths = [[0 for x in range(n)] for y in range(m)]
-    paths[0][0] = 1
+    maxProfit = [[0 for x in range(n)] for y in range(m)]
+    maxProfit[0][0] = 1
     
     for x in range(m):
         for y in range(n):          
-                                      
+            maxProfit[x][y] = profits[x][y]
+              
             if (x > 0 and y > 0):
-                paths[x][y] = Cell(x - 1, y, paths, obstacles) + Cell(x, y - 1, paths, obstacles)
+                maxProfit[x][y] += max(maxProfit[x - 1][y], maxProfit[x][y - 1])
             elif (x > 0):
-                paths[x][y] = Cell(x - 1, y, paths, obstacles)
+                maxProfit[x][y] += maxProfit[x - 1][y]
             elif (y > 0):
-                paths[x][y] = Cell(x, y - 1, paths, obstacles)
+                maxProfit[x][y] += maxProfit[x][y - 1]                
                 
-    return paths[m - 1][n - 1]
+    return maxProfit[m - 1][n - 1]
 
-
-obstacles = [[0 for x in range(10)] for y in range(10)]
-obstacles[1][2] = 1
-obstacles[1][3] = 1
-obstacles[2][1] = 1
-obstacles[3][3] = 1
-obstacles[3][3] = 1
-
-print(MatrixNumberOfPaths(1, 1, obstacles)) # 1           
-print(MatrixNumberOfPaths(2, 2, obstacles)) # 2
-print(MatrixNumberOfPaths(3, 3, obstacles)) # 0
-print(MatrixNumberOfPaths(4, 4, obstacles)) # 1
-print(MatrixNumberOfPaths(4, 5, obstacles)) # 1
-print(MatrixNumberOfPaths(5, 4, obstacles)) # 3
-print(MatrixNumberOfPaths(5, 5, obstacles)) # 4
+class TestCases(unittest.TestCase):
+    def setUp(self):
+        self.profits = [[0 for x in range(10)] for y in range(10)]
+        self.profits[0][0] = 3
+        self.profits[0][1] = 2
+        self.profits[1][0] = 4
+        self.profits[1][1] = 1
+        self.profits[1][2] = 5
+        self.profits[2][1] = 5
+        self.profits[2][2] = 2
+        self.profits[3][3] = 3
+        self.profits[4][4] = 4
+        self.profits[1][3] = 1
+        self.profits[3][1] = 4
+        self.profits[3][2] = 4
+        self.profits[2][3] = 4
+        self.profits[3][4] = 6
+        self.profits[4][3] = 6        
+        
+    def testCase1(self):        
+        self.assertEqual(MatrixPathMaximumProfit(1, 1, self.profits), 3, "Should be 3")
+        
+    def testCase2(self):        
+        self.assertEqual(MatrixPathMaximumProfit(2, 2, self.profits), 8, "Should be 8")
+        
+    def testCase3(self):
+        self.assertEqual(MatrixPathMaximumProfit(3, 3, self.profits), 15, "Should be 15")
+        
+    def testCase4(self):        
+        self.assertEqual(MatrixPathMaximumProfit(4, 4, self.profits), 24, "Should be 24")
+        
+    def testCase5(self):        
+        self.assertEqual(MatrixPathMaximumProfit(4, 5, self.profits), 30, "Should be 30")
+        
+    def testCase6(self):        
+        self.assertEqual(MatrixPathMaximumProfit(5, 4, self.profits), 30, "Should be 30")
+        
+    def testCase7(self):        
+        self.assertEqual(MatrixPathMaximumProfit(5, 5, self.profits), 34, "Should be 34")
