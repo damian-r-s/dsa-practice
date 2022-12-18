d92076d7497447bf00ce63636af43169287d516c
Damian Sobanski <damian.flexsoft@gmail.com>
2022-12-18 23:25:55 +0100

    Calculate maximum profit

--- Python/DynamicProgramming/Matrix/MatrixNumberOfPathsWithObstacles.py
+++ Python/DynamicProgramming/Matrix/MatrixNumberOfPathsWithObstacles.py
@@ -1,37 +1,44 @@
-def MatrixNumberOfPaths(m, n, obstacles):
+def MatrixNumberOfPaths(m, n, profits):
     if m == 1 and n == 1:
-        return 1
+        return profits[0][0]
     
-    paths = [[0 for x in range(n)] for y in range(m)]
-    paths[0][0] = 1
+    maxProfit = [[0 for x in range(n)] for y in range(m)]
+    maxProfit[0][0] = 1
     
     for x in range(m):
-        for y in range(n):                        
+        for y in range(n):          
+            maxProfit[x][y] = profits[x][y]
+              
             if (x > 0 and y > 0):
-                paths[x][y] = Cell(x - 1, y, paths, obstacles) + Cell(x, y - 1, paths, obstacles)
+                maxProfit[x][y] += max(maxProfit[x - 1][y], maxProfit[x][y - 1])
             elif (x > 0):
-                paths[x][y] = Cell(x - 1, y, paths, obstacles)
+                maxProfit[x][y] += maxProfit[x - 1][y]
             elif (y > 0):
-                paths[x][y] = Cell(x, y - 1, paths, obstacles)
+                maxProfit[x][y] += maxProfit[x][y - 1]                
                 
-    return paths[m - 1][n - 1]
+    return maxProfit[m - 1][n - 1]
 
-def Cell(x, y, paths, obstacles):
-    if (obstacles[x][y] == 0):
-        return paths[x][y]    
-    return 0
+profits = [[0 for x in range(10)] for y in range(10)]
+profits[0][0] = 3
+profits[0][1] = 2
+profits[1][0] = 4
+profits[1][1] = 1
+profits[1][2] = 5
+profits[2][1] = 5
+profits[2][2] = 2
+profits[3][3] = 3
+profits[4][4] = 4
+profits[1][3] = 1
+profits[3][1] = 4
+profits[3][2] = 4
+profits[2][3] = 4
+profits[3][4] = 6
+profits[4][3] = 6
 
-obstacles = [[0 for x in range(10)] for y in range(10)]
-obstacles[1][2] = 1
-obstacles[1][3] = 1
-obstacles[2][1] = 1
-obstacles[3][3] = 1
-obstacles[3][3] = 1
-
-print(MatrixNumberOfPaths(1, 1, obstacles)) # 1           
-print(MatrixNumberOfPaths(2, 2, obstacles)) # 2
-print(MatrixNumberOfPaths(3, 3, obstacles)) # 0
-print(MatrixNumberOfPaths(4, 4, obstacles)) # 1
-print(MatrixNumberOfPaths(4, 5, obstacles)) # 1
-print(MatrixNumberOfPaths(5, 4, obstacles)) # 3
-print(MatrixNumberOfPaths(5, 5, obstacles)) # 4
+print(MatrixNumberOfPaths(1, 1, profits)) # 3           
+print(MatrixNumberOfPaths(2, 2, profits)) # 2
+print(MatrixNumberOfPaths(3, 3, profits)) # 0
+print(MatrixNumberOfPaths(4, 4, profits)) # 1
+print(MatrixNumberOfPaths(4, 5, profits)) # 1
+print(MatrixNumberOfPaths(5, 4, profits)) # 3
+print(MatrixNumberOfPaths(5, 5, profits)) # 4
