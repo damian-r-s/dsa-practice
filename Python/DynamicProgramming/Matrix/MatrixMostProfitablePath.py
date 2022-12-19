ccb7475b893870ad896c82b774e081aed4d94bd6
Damian Sobanski <damian.flexsoft@gmail.com>
2022-12-19 21:00:50 +0100

    Matrix most profitable path with path reconstruction

--- Python/DynamicProgramming/Matrix/MatrixMostProfitablePath.py
+++ Python/DynamicProgramming/Matrix/MatrixMostProfitablePath.py
@@ -2,7 +2,7 @@ import unittest
 
 def MatrixPathMaximumProfit(m, n, profits):
     if m == 1 and n == 1:
-        return profits[0][0]
+        return (profits[0][0], [(0, 0)])
     
     maxProfit = [[0 for x in range(n)] for y in range(m)]
     maxProfit[0][0] = 1
@@ -18,7 +18,22 @@ def MatrixPathMaximumProfit(m, n, profits):
             elif (y > 0):
                 maxProfit[x][y] += maxProfit[x][y - 1]                
                 
-    return maxProfit[m - 1][n - 1]
+    maxProfitablePath = calculatePath(maxProfit, m - 1, n - 1)
+    return (maxProfit[m - 1][n - 1], maxProfitablePath)
+
+def calculatePath(maxProfit, x, y):
+    if x == 0 and y == 0:
+        return [(0, 0)]
+    elif x == 0:
+        return calculatePath(maxProfit, x, y - 1) + [(x, y)]
+    elif y == 0:
+        return calculatePath(maxProfit, x - 1, y) + [(x, y)]
+    else:
+        if maxProfit[x - 1][y] > maxProfit[x][y - 1]:
+            return calculatePath(maxProfit, x - 1, y) + [(x, y)]
+        else:
+            return calculatePath(maxProfit, x, y - 1) + [(x, y)]
+    
 
 class TestCases(unittest.TestCase):
     def setUp(self):
@@ -39,23 +54,30 @@ class TestCases(unittest.TestCase):
         self.profits[3][4] = 6
         self.profits[4][3] = 6        
         
-    def testCase1(self):        
-        self.assertEqual(MatrixPathMaximumProfit(1, 1, self.profits), 3, "Should be 3")
+    def testCase1(self): 
+        result = MatrixPathMaximumProfit(1, 1, self.profits)               
+        self.assertEqual(result[0], 3, "Should be 3")
         
-    def testCase2(self):        
-        self.assertEqual(MatrixPathMaximumProfit(2, 2, self.profits), 8, "Should be 8")
+    def testCase2(self): 
+        result = MatrixPathMaximumProfit(2, 2, self.profits)       
+        self.assertEqual(result[0], 8, "Should be 8")
         
     def testCase3(self):
-        self.assertEqual(MatrixPathMaximumProfit(3, 3, self.profits), 15, "Should be 15")
+        result = MatrixPathMaximumProfit(3, 3, self.profits)
+        self.assertEqual(result[0], 15, "Should be 15")
         
     def testCase4(self):        
-        self.assertEqual(MatrixPathMaximumProfit(4, 4, self.profits), 24, "Should be 24")
+        result = MatrixPathMaximumProfit(4, 4, self.profits)
+        self.assertEqual(result[0], 24, "Should be 24")
         
     def testCase5(self):        
-        self.assertEqual(MatrixPathMaximumProfit(4, 5, self.profits), 30, "Should be 30")
+        result = MatrixPathMaximumProfit(4, 5, self.profits)
+        self.assertEqual(result[0], 30, "Should be 30")
         
     def testCase6(self):        
-        self.assertEqual(MatrixPathMaximumProfit(5, 4, self.profits), 30, "Should be 30")
+        result = MatrixPathMaximumProfit(5, 4, self.profits)
+        self.assertEqual(result[0], 30, "Should be 30")
         
     def testCase7(self):        
-        self.assertEqual(MatrixPathMaximumProfit(5, 5, self.profits), 34, "Should be 34")
+        result = MatrixPathMaximumProfit(5, 5, self.profits)
+        self.assertEqual(result[0], 34, "Should be 34")
