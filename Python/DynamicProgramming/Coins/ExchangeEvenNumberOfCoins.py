e116536668aea589a691ae043f0ac1d0d6009a57
Damian Sobanski <d.sobanski@hotmail.com>
2022-12-26 21:55:34 +0100

    Find number of ways to split n, with available coins, evenly

---
+++ Python/DynamicProgramming/Coins/ExchangeEvenNumberOfCoins.py
@@ -0,0 +1,39 @@
+import unittest
+
+# 1 - even
+# 0 - odd
+def ExchangeEvenNumberOfCoins(coins, n):
+    
+    result = [[0 for y in range(2)] for x in range(n + 1)]    
+    result[0][1] = 1
+    
+    for i in range(1, n + 1):
+        for c in coins:
+            if i - c >= 0:
+                result[i][0] += result[i - c][1]
+                result[i][1] += result[i - c][0]
+    
+    return result[n][1]    
+
+print(ExchangeEvenNumberOfCoins([1, 3, 5, 10], 10))    
+
+class TestCase(unittest.TestCase):
+    def testCase1(self):
+        result = ExchangeEvenNumberOfCoins([1, 3, 5, 10], 1)
+        self.assertEqual(result, 0, "Should be 0")
+        
+    def testCase2(self):
+        result = ExchangeEvenNumberOfCoins([1, 3, 5, 10], 10)
+        self.assertEqual(result, 47, "Should be 47")
+        
+    def testCase3(self):
+        result = ExchangeEvenNumberOfCoins([1, 3, 5, 10], 4)
+        self.assertEqual(result, 3, "Should be 3")
+        
+    def testCase4(self):
+        result = ExchangeEvenNumberOfCoins([1, 3, 5, 10], 6)
+        self.assertEqual(result, 8, "Should be 8")
+        
+    # def testCase5(self):
+    #     result = ExchangeEvenNumberOfCoins([1, 3, 5, 10], 10)
+    #     self.assertEqual(result, 48, "Should be 48")
