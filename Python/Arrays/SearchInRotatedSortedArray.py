d8d4b5dd4bba70ce5e706dd983796bcfcb8377d3
Damian Sobanski <damian.flexsoft@gmail.com>
2023-01-17 22:00:20 +0100

    Search in rotated array simplified

--- Python/Arrays/SearchInRotatedSortedArray.py
+++ Python/Arrays/SearchInRotatedSortedArray.py
@@ -1,40 +1,23 @@
 def search(nums, target):
-    i = 0
-    j = len(nums) - 1
-    a = nums[i]
-    b = nums[j]        
-            
-    while i <= j:
-        mid = (i + j) // 2                         
-        a = nums[i]
-        b = nums[j]
-        c = nums[mid]
+    left = 0
+    right = len(nums) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
         
-        if target == c:
+        if nums[mid] == target:
             return mid
-        if target == a:
-            return i
-        if target == b:
-            return j
-                    
-        if a <= b:
-            if target < c:
-                j = mid - 1
+        elif nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
             else:
-                i = mid + 1            
-        elif a > b:
-            if a < c and target > a and target < c:
-                j = mid - 1
-            elif c < b and target > c and target < b:
-                i = mid + 1                
-            elif a > c and (target > a or target < c):
-                j = mid - 1                
-            elif c > b and (target > c or target < b):
-                i = mid + 1
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
             else:
-                i = mid + 1
-            
+                right = mid - 1
+                
     return -1
-
 
 print(search([4,5,6,7,0,1,2], 3))
