a682df0253b72a8a71558f6f83d8af6a1f91da25
Damian Sobanski <damian.flexsoft@gmail.com>
2023-02-14 23:17:09 +0100

    Simple hash set

---
+++ CSharp/Hash/HashSet.cs
@@ -0,0 +1,44 @@
+public class MyHashSet
+{
+    private List<int>[] _array;
+    public MyHashSet() => _array = new List<int>[1000000];
+    private int Hash(int key) => key % 7907;
+    public void Add(int key)
+    {
+        var list = _array[Hash(key)];
+        if (list == null)
+        {
+            list = _array[Hash(key)] = new List<int>();
+            list.Add(key);
+            return;
+        }
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i] == key)
+                return;
+        }
+        list.Add(key);
+    }
+
+    public void Remove(int key)
+    {
+        var list = _array[Hash(key)];
+        if (list == null)
+            return;
+        list.Remove(key);
+    }
+
+    public bool Contains(int key)
+    {
+        var list = _array[Hash(key)];
+        if (list == null)
+            return false;
+
+        for (int i = 0; i < list.Count; i++)
+            if (list[i] == key)
+                return true;
+
+        return false;
+    }
+}
