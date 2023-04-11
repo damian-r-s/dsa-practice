b49b748f07892ce7e2ecc13e64ab0d4e7169b09a
Damian <damian.flexsoft@gmail.com>
2023-04-11 14:14:19 +0200

    Undirected connected components

---
+++ Python/Graphs/Bfs/UCC.py
@@ -0,0 +1,28 @@
+#Undirected Connected components on BFS
+import queue
+
+def UCC(graph):    
+    q = queue.Queue()    
+    length = len(graph)
+    visited = [0 for i in range(length)]
+    componetns = [0 for i in range(length)]
+    ucc = 0    
+    
+    for vertex in range(length):
+        if visited[vertex] == 1:
+            continue        
+        ucc += 1
+        q.put(vertex)        
+        while q.empty() == False:
+            s = q.get()
+            if visited[s] == 1:
+                continue
+            visited[s] = 1
+            componetns[s] = ucc            
+            for v in graph[s]:
+                q.put(v)        
+        
+    return componetns
+
+print(UCC([[1], [0, 2], [3, 1], [2]]))
+
