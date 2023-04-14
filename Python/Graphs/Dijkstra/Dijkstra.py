140705d61121e9c022645b4744967ddeac19363b
Damian <damian.flexsoft@gmail.com>
2023-04-14 21:43:10 +0200

    Python dijkstra, naitve implementation

---
+++ Python/Graphs/Dijkstra/Dijkstra.py
@@ -0,0 +1,33 @@
+def findClosestVertex(distances, visited):
+    min = 1e7
+    result = -1    
+    
+    for idx in range(len(distances)):
+        if distances[idx] < min and visited[idx] == False:
+            min = distances[idx]
+            result = idx
+    
+    return result
+
+def dijkstraNaive(graph, s):
+    n = len(graph)
+    vistied = [False] * n
+    distances = [1e7] * n    
+    distances[s] = 0
+    
+    for i in range(n):
+        idx = findClosestVertex(distances, vistied)        
+        vistied[idx] = True
+                
+        for adjV in graph[idx]:
+            vertex = adjV[0]
+            length = adjV[1]
+            
+            if vistied[vertex] == False and distances[vertex] > (distances[idx] + length):
+                distances[vertex] = distances[idx] + length
+        
+    return distances
+    
+graph = [[(1, 2), (2, 3)], [(3, 1)], [(3, 4)], []]
+
+print(dijkstraNaive(graph, 0))
