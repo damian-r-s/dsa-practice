10705159e7eae96b7db7946373623ff5c0726373
Damian <damian.flexsoft@gmail.com>
2023-05-16 18:22:51 +0200

    Missing numbers in C language

---
+++ Applications/Cpp/MissingNumber.c
@@ -0,0 +1,15 @@
+
+int missingNumber(int *nums, int numsSize)
+{
+    int sum = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        sum += nums[i];
+    }
+    return numsSize * (numsSize + 1) / 2 - sum;
+}
+
+int main(void)
+{
+    return 0;
+}
