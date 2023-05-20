b986bb9600b6b84be8ec2215f3f3d0f7ade284bb
Damian <damian.flexsoft@gmail.com>
2023-05-20 19:13:34 +0200

    Two Sum

---
+++ Applications/Python/Arrays/TwoSum.py
@@ -0,0 +1,16 @@
+# 1. Two Sum Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    numbers = {}
+
+    for n in range(len(nums)):
+        diff = target - nums[n]
+
+        if diff in numbers:
+            return [n, numbers[diff]]        
+        
+        numbers[nums[n]] = n
+    
+    return []
