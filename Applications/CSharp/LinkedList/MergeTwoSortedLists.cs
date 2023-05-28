2b0e93eaef15aafbcc4492290b174a4f514b8d37
Damian <damian.flexsoft@gmail.com>
2023-05-28 18:56:28 +0200

    Merge two sorted lists C# Language

---
+++ Applications/CSharp/LinkedList/MergeTwoSortedLists.cs
@@ -0,0 +1,51 @@
+// 21. Merge Two Sorted Lists
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+
+class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+class Solution
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        var head = new ListNode(0);
+
+        var current = head;
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val < list2.val)
+            {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        if (list1 != null)
+        {
+            current.next = list1;
+        }
+
+        if (list2 != null)
+        {
+            current.next = list2;
+        }
+
+        return head.next;
+    }
+}
