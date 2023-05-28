73397097f4b93fed813920c4040f397a01914021
Damian <damian.flexsoft@gmail.com>
2023-05-28 19:54:54 +0200

    Remove N-th node from Linked List in Python Language

---
+++ Applications/Python/LinkedList/RemoveNthNodeFromLL.py
@@ -0,0 +1,23 @@
+# 19. Remove Nth Node From End of List
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        # delete
+        left.next = left.next.next
+        return dummy.next
---
+++ Applications/Python/LinkedList/ReorderLinkedList.py
@@ -0,0 +1,37 @@
+# 143. Reorder List
+
+# Definition for singly-linked list.
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def reorderList(self, head):
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if head is None or head.next is None:
+            return
+
+        result = head.next
+        lst = []
+
+        while(result != None):
+            lst.append(result)
+            result = result.next
+
+
+        l = 0
+        r = len(lst) - 1
+        result = head
+        while (l <= r):
+            result.next = lst[r]
+            result.next.next = lst[l]
+            result = result.next.next
+
+            r -= 1
+            l += 1
+
+        result.next = None
