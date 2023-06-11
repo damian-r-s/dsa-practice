e023aec05aedeb477a58ad862f592ca761b57e99
Damian <damian.flexsoft@gmail.com>
2023-06-11 13:19:44 +0200

    Jump game 2 furtherst soluiton in Python

--- Applications/Python/Greedy/JumpGame.py
+++ Applications/Python/Greedy/JumpGame.py
@@ -2,18 +2,20 @@
 # You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
 # Return true if you can reach the last index, or false otherwise.
 
-class Solution:   
-    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key =  lambda row: row[0])
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums) - 1
+        g = n
 
-        result = 0
-        prev = intervals[0][1]
+        for i in range(n, -1, -1):
+            current = nums[i]
 
-        for s, e in intervals[1:]:
-            if s >= prev:
-                prev = e
-            else:
-                result += 1
-                prev = min(prev, e)
+            if i + current >= g:
+                g = i
 
-        return result
+
+        if g == 0:
+            return True
+
+        return False
+
