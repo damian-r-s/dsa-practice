ecea733b6a23646647639cfd751c7066defa7201
Damian <damian.flexsoft@gmail.com>
2023-07-15 22:33:55 +0200

    Min cost climbing stairs improvment in Python

--- Applications/Python/DynamicProgramming/Stairs/MinCostClimbingStairsBottomUp.py
+++ Applications/Python/DynamicProgramming/Stairs/MinCostClimbingStairsBottomUp.py
@@ -5,23 +5,10 @@
 
 class Solution:    
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        if n <= 1:
-            return cost[0]
-
-        result = [9999 for x in range(n + 1)]
-        result[0] = 0
-        result[1] = cost[0]
-        
-        for stair in range(2, n + 1, 1):                                    
-            step1 = result[stair - 1]
-            if (n + 1 - stair) > 1:
-                step1 +=  cost[stair - 1]            
-
-            step2 = result[stair - 2] + cost[stair - 1]            
-            result[stair] = min(step1, step2)
-
-        return result[n]
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+                        
+        return min(cost[0], cost[1])
 
 
 
