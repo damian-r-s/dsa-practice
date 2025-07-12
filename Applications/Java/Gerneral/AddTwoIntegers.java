36f8ea42750778e2bc2fbd7b8e6090b5d248cbc1
damian-r-s <d.sobanski@hotmail.com>
2025-07-12 14:33:40 +0200

    Java, naive LeetCode example

---
+++ Applications/Java/Gerneral/AddTwoIntegers.java
@@ -0,0 +1,10 @@
+package Applications.Java.Gerneral;
+
+public class AddTwoIntegers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }   
+}
