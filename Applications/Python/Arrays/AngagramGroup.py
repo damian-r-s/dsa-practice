da417cda8e8266bebe4f7f2611873680f07848e8
Damian Sobanski <d.sobanski@hotmail.com>
2026-07-26 18:14:09 +0200

    49. group anagrams

---
+++ Applications/Python/Arrays/AngagramGroup.py
@@ -0,0 +1,54 @@
+# 49. Group Anagrams
+# Given an array of strings strs, group the together. You can return the answer in any order.
+
+# Example 1:
+
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+# Explanation:
+
+#     There is no string in strs that can be rearranged to form "bat".
+#     The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+#     The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+# Example 2:
+
+# Input: strs = [""]
+
+# Output: [[""]]
+
+# Example 3:
+
+# Input: strs = ["a"]
+
+# Output: [["a"]] 
+
+# Constraints:
+
+#     1 <= strs.length <= 104
+#     0 <= strs[i].length <= 100
+#     strs[i] consists of lowercase English letters.
+
+
+from ast import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:        
+        dic = {}
+        
+        for s in strs:            
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - 97]  += 1
+
+            key = tuple(count)
+
+            if key not in dic:
+                dic[key] = []
+
+            dic[key].append(s)
+
+        return list(dic.values())
